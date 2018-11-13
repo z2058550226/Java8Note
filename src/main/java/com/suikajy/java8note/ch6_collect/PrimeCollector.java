2b7c7febbe6796ad90f9109da28eac36ac32f0fa
suikajy <2058550226@qq.com>
2018-11-13 16:56:17 +0800

    ch6 collect

--- src/main/java/com/suikajy/java8note/ch6_collect/PrimeCollector.java
+++ src/main/java/com/suikajy/java8note/ch6_collect/PrimeCollector.java
@@ -1,39 +1,69 @@
 package com.suikajy.java8note.ch6_collect;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.*;
+import java.util.function.*;
 import java.util.stream.Collector;
 
-public class PrimeCollector implements Collector<Integer, Map<Boolean, List<Integer>>,Map<Boolean,List<Integer>>> {
+import static java.util.stream.Collector.Characteristics.*;
+
+public class PrimeCollector implements Collector<Integer, Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> {
 
     @Override
     public Supplier<Map<Boolean, List<Integer>>> supplier() {
-        return HashMap::new;
+        return () -> {
+            Map<Boolean, List<Integer>> map = new HashMap<>();
+            map.put(true, new ArrayList<>());
+            map.put(false, new ArrayList<>());
+            return map;
+        };
     }
 
     @Override
     public BiConsumer<Map<Boolean, List<Integer>>, Integer> accumulator() {
-        return null;
+        return (map, i) -> map
+                .get(isPrime(map.get(true), i))
+                .add(i);
     }
 
     @Override
     public BinaryOperator<Map<Boolean, List<Integer>>> combiner() {
-        return null;
+        return (map1, map2) -> {
+            map1.get(true).addAll(map2.get(true));
+            map2.get(false).addAll(map2.get(false));
+            return map1;
+        };
     }
 
     @Override
     public Function<Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> finisher() {
-        return null;
+        return Function.identity();
     }
 
     @Override
     public Set<Characteristics> characteristics() {
-        return null;
+        return Collections.unmodifiableSet(EnumSet.of(IDENTITY_FINISH));
+    }
+
+    private boolean isPrime(List<Integer> acc, Integer candidate) {
+        int candidateRoot = (int) Math.sqrt(candidate);
+        return takeWhile(acc, i -> i < candidateRoot)
+                .stream()
+                .noneMatch(i -> candidate % i == 0);
+
+//        return acc.stream()
+//                .filter(i -> i < candidateRoot)
+//                .noneMatch(i -> candidate % i == 0);
+    }
+
+    // 使用takeWhile来加速filter。因为filter会对流中的每一个数字判断，但其实到了候选数字的根即可停止了。
+    private static <A> List<A> takeWhile(List<A> list, Predicate<A> p) {
+        int i = 0;
+        for (A item : list) {
+            if (!p.test(item)) {
+                return list.subList(0, i);
+            }
+            i++;
+        }
+        return list;
     }
 }
--- src/main/java/com/suikajy/java8note/ch6_collect/Test1.java
+++ src/main/java/com/suikajy/java8note/ch6_collect/Test1.java
@@ -1,8 +1,21 @@
 package com.suikajy.java8note.ch6_collect;
 
+import java.util.stream.IntStream;
+
 public class Test1 {
-    
+
     public static void main(String[] args) {
-        
+//        IntStream.rangeClosed(2, 100).filter(i -> {
+//            int iRoot = (int) Math.sqrt(i);
+//            return IntStream.rangeClosed(2, iRoot)
+//                    .noneMatch(divider -> i % divider == 0);
+//        }).forEach(System.out::println);
+
+        IntStream.rangeClosed(2, 100).boxed()
+                .collect(new PrimeCollector())
+                .get(true)
+                .forEach(System.out::println);
     }
+
+
 }
